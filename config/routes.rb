9b86e596f7d4fde27d9c230d4982c4c37d113704
Jorge Pioquinto <129575128+Jorge-Pioquinto@users.noreply.github.com>
2023-09-27 20:49:33 +0000

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-get("/", {controller:"pages", action:"home"})
+  get("/", {:controller =>"pages", :action => "home"})
+
+  get("/dice/:number_of_dice/:how_many_sides", {:controller =>"pages", :action => "flexible"})
+
 end
